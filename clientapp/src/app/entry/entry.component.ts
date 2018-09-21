ee633d1fedd1ed875aca46aa332d3eeffba09d74
Nathan Files <nathanwfiles@gmail.com>
2018-09-21 17:11:03 -0400

    signed commit

--- clientapp/src/app/entry/entry.component.ts
+++ clientapp/src/app/entry/entry.component.ts
@@ -1,23 +1,19 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormControl, NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.css'],
 })
-export class EntryComponent implements OnInit {
-  public form: FormGroup;
+export class EntryComponent {
+  form = new FormGroup({
+    vehicleId: new FormControl(null),
+    date: new FormControl(''),
+    miles: new FormControl(null),
+    gallons: new FormControl(null),
+    comments: new FormControl(''),
+  });
 
-  constructor() {
-    this.form = new FormGroup({
-      date: new FormControl(''),
-      miles: new FormControl(null),
-      gallons: new FormControl(null),
-    });
-  }
-
-  ngOnInit() {
-    console.log(this.form);
-  }
+  constructor() {}
 }
--- functions/record-mileage/index.js
+++ functions/record-mileage/index.js
@@ -1,20 +1,76 @@
-module.exports = function(context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
+const azure = require('azure-storage');
 
-  // TODO: add an entry to the storage container? table?
+const connectionString = [
+  'DefaultEndpointsProtocol=http',
+  'AccountName=devstoreaccount1',
+  'AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==',
+  'BlobEndpoint=http://storage:10000/devstoreaccount1',
+  'QueueEndpoint=http://storage:10001/devstoreaccount1',
+  'TableEndpoint=http://storage:10002/devstoreaccount1',
+].join(';');
 
-  const name = req.query.name || (req.body && req.body.name);
+const containerName = 'mileage';
+const driversTableName = 'driversTable';
+const mileageTableName = 'mileageTable';
 
-  if (name) {
-    context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: "Hello " + (req.query.name || req.body.name)
-    };
-  } else {
-    context.res = {
-      status: 400,
-      body: "Please pass a name on the query string or in the request body"
-    };
+const blobService = azure.createBlobService(connectionString);
+const tableService = azure.createTableService(connectionString);
+
+module.exports = async function(context, req) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  // date
+  const vehicleId = String(req.query.vehicleId || '');
+  const dateString = String(req.query.date || '');
+  const entryDate = new Date(dateString);
+  const miles = Number(req.query.miles);
+  const gallons = Number(req.query.gallons);
+  const comments = String(req.body.comments || '');
+
+  if (
+    !vehicleId ||
+    !dateString ||
+    entryDate.toString() === 'Invalid Date' ||
+    !(miles > 0) ||
+    !(gallons > 0)
+  ) {
+    context.res = { status: 400 };
+    context.done();
+    return;
   }
+
+  try {
+    // TODO: add an entry to the storage container? table?
+
+    /** @type {azure.services.table.TableService.TableResult} */
+    const tableResult = await new Promise((resolve, reject) => {
+      tableService.createTableIfNotExists(
+        mileageTableName,
+        (err, table) => (err ? reject(err) : resolve(table))
+      );
+    });
+
+    const entGen = azure.TableUtilities.entityGenerator;
+    const entry = {
+      PartitionKey: entGen.Guid(vehicleId),
+      RowKey: entGen.DateTime(entryDate),
+      miles: entGen.Double(miles),
+      gallons: entGen.Double(gallons),
+    };
+
+    /** @type {azure.services.table.TableService.EntityMetadata} */
+    const result = await new Promise((resolve, reject) => {
+      tableService.insertEntity(
+        mileageTableName,
+        entry,
+        (err, meta) => (err ? reject(err) : resolve(meta))
+      );
+    });
+
+    context.res = { status: 200 };
+  } catch (err) {
+    context.res = { status: 500, body: err };
+  }
+
   context.done();
 };
