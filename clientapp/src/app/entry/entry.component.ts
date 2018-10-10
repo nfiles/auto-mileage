967f433a3082785b28a5082c0690840104faaee3
Nathan Files <nathanwfiles@gmail.com>
2018-10-10 18:52:51 -0400

    update client app

--- clientapp/src/app/entry/entry.component.ts
+++ clientapp/src/app/entry/entry.component.ts
@@ -1,23 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormControl, NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.css'],
 })
-export class EntryComponent implements OnInit {
-  public form: FormGroup;
-
-  constructor() {
-    this.form = new FormGroup({
-      date: new FormControl(''),
-      miles: new FormControl(null),
-      gallons: new FormControl(null),
-    });
-  }
-
-  ngOnInit() {
-    console.log(this.form);
-  }
+export class EntryComponent {
+  form = new FormGroup({
+    vehicleId: new FormControl(null),
+    date: new FormControl(''),
+    miles: new FormControl(null),
+    gallons: new FormControl(null),
+    comments: new FormControl(''),
+  });
 }
