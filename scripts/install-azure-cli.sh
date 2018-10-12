db77ffd3828aaceec925f4956ae5976de1326fd5
Nathan Files <nathanwfiles@gmail.com>
2018-10-12 14:09:11 -0400

    try to clean up travis config

--- scripts/install-azure-cli.sh
+++ scripts/install-azure-cli.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-# curl -L https://aka.ms/InstallAzureCli | bash
+curl -L https://aka.ms/InstallAzureCli | bash
 
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt-get install apt-transport-https
-sudo apt-get update && sudo apt-get install azure-cli
+# AZ_REPO=$(lsb_release -cs)
+# echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+# curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+# sudo apt-get install apt-transport-https
+# sudo apt-get update && sudo apt-get install azure-cli
--- scripts/install-func-tools.sh
+++ scripts/install-func-tools.sh
@@ -3,14 +3,10 @@
 URL="https://github.com/Azure/azure-functions-core-tools/releases/download/2.0.3/Azure.Functions.Cli.linux-x64.2.0.3.zip"
 DEST="/azure-functions-cli"
 
-echo "downloading package"
 wget -q "$URL"
 
-echo "unpacking package"
 mkdir "$DEST"
 unzip -q -d "$DEST" Azure.Functions.Cli.linux-x64.*.zip
 
-echo "making binary executable"
 chmod +x "$DEST/func"
-echo "linking binary to /usr/local/bin"
-ln -s "$DEST/func" /usr/local/bin/func
+export PATH=$DEST/func:$PATH
