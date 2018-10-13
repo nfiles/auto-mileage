50ce0b497b33d18816fc7e3761a37a14dab084e8
Nathan Files <nathanwfiles@gmail.com>
2018-10-13 19:40:02 -0400

    adds debug logging to travis deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 
 # authenticate with azure
+echo az login
+echo     --service-principal
+echo     -u "$CICD_AUTH_NAME"
+echo     -p "$CICD_AUTH_PASSWORD"
+echo     --tenant "$CICD_AUTH_TENANT"
 az login \
     --service-principal \
     -u "$CICD_AUTH_NAME" \
     -p "$CICD_AUTH_PASSWORD" \
-    --tenant "$CICD_AUTH_TENANT"
+    --tenant "$CICD_AUTH_TENANT" \
+    || exit $?
 
 # publish the function app
-echo "func path: $(which func)"
 pushd ./functions
-echo "func path: $(which func)"
 func azure functionapp publish "$FUNCTIONAPP_NAME" || exit $?
 popd
 
