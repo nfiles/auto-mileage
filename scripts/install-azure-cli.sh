44c78e903d9f1fa26d41d2794c56f2bf340922d2
Nathan Files <nathanwfiles@gmail.com>
2018-10-12 23:13:51 -0400

    moves az/func setup to deploy step

--- scripts/install-azure-cli.sh
+++ scripts/install-azure-cli.sh
@@ -3,6 +3,5 @@
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt-get install apt-transport-https --no-install-recommends -y
 sudo apt-get update -qq
 sudo apt-get install azure-cli --no-install-recommends -y
