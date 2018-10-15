e7d117117276f92790f33ed533fd20396693f000
Nathan Files <nathanwfiles@gmail.com>
2018-10-15 17:05:24 -0400

    replace most of build process with docker

--- scripts/install-azure-cli.sh
+++ scripts/install-azure-cli.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
-function sudo {
-    $*
-}
-
 AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt-get update -qq
-sudo apt-get install azure-cli --no-install-recommends -y
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | tee /etc/apt/sources.list.d/azure-cli.list
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+apt-get update -qq
+apt-get install azure-cli --no-install-recommends -y
+
+echo az --version
+az --version
--- scripts/install-func-tools.sh
+++ scripts/install-func-tools.sh
@@ -1,13 +1,9 @@
 #! /bin/bash
 
-function sudo {
-    $*
-}
-
 VERSION_NUMBER="$(lsb_release -rs)"
-URL="https://packages.microsoft.com/config/ubuntu/$VERSION_NUMBER/packages-microsoft-prod.deb"
-wget -q "$URL"
-sudo dpkg -i packages-microsoft-prod.deb
+wget -q "https://packages.microsoft.com/config/ubuntu/$VERSION_NUMBER/packages-microsoft-prod.deb"
+dpkg -i packages-microsoft-prod.deb
+apt-get update -qq
+apt-get install azure-functions-core-tools --no-install-recommends -y
 
-sudo apt-get update
-sudo apt-get install azure-functions-core-tools --no-install-recommends -y
+func --version
